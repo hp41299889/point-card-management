a09db47f3453f8cd1142695f5ceccc3e4e7f23f1
hp4129889 <hp4129889@gmail.com>
2023-08-22 15:46:58 +0800

    refactor: extract form from table

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Divider, Typography } from "@mui/material";
+import { useState } from "react";
+import { Box, CircularProgress, Divider, Typography } from "@mui/material";
 
 import { TableMetadata } from "@/components/table/interface";
 import { toLocaleDateTime } from "@/utils/time";
 import ManagementTable from "@/components/table/ManagementTable";
 import UserForm from "@/components/form/management/UserForm";
 import { useUsers } from "@/components/table/hook";
+import { User } from "../api/user/interface";
+import { FormType } from "@/components/form/management/interface";
+
+type Data = User | null;
 
 const metadata: TableMetadata[] = [
   {
@@ -29,15 +34,65 @@ const metadata: TableMetadata[] = [
 ];
 
 const Page = () => {
+  const { data, fetcher, loading } = useUsers();
+  const [selected, setSelected] = useState<Data>(null);
+  const [formType, setFormType] = useState<FormType>("create");
+  const [formModal, setFormModal] = useState<boolean>(false);
+
+  const onClose = () => {
+    setFormModal(false);
+    setSelected(null);
+  };
+
+  const onClickNewData = () => {
+    setFormType("create");
+    setSelected(null);
+    setFormModal(true);
+  };
+
+  const onClickWatchData = (data: Data) => {
+    setFormType("watch");
+    setSelected(data);
+    setFormModal(true);
+  };
+
+  const onClickEditData = (data: Data) => {
+    setFormType("edit");
+    setSelected(data);
+    setFormModal(true);
+  };
+
+  const onClickDeleteData = (data: Data) => {
+    setFormType("delete");
+    setSelected(data);
+    setFormModal(true);
+  };
+
   return (
     <Box>
       <Typography variant="h6">管理員</Typography>
       <Divider />
-      <ManagementTable
-        title="用戶管理"
-        metadata={metadata}
-        useData={useUsers}
-        Form={UserForm}
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <ManagementTable<User>
+          title="用戶管理"
+          metadata={metadata}
+          datas={data}
+          onClickData={{
+            onNew: onClickNewData,
+            onWatch: onClickWatchData,
+            onEdit: onClickEditData,
+            onDelete: onClickDeleteData,
+          }}
+        />
+      )}
+      <UserForm
+        open={formModal}
+        type={formType}
+        data={selected}
+        onClose={onClose}
+        afterAction={fetcher}
       />
     </Box>
   );
--- pages/customer/index.tsx
+++ pages/customer/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Divider, Typography } from "@mui/material";
+import { useState } from "react";
+import { Box, Divider, Typography, CircularProgress } from "@mui/material";
 
 import ManagementTable from "@/components/table/ManagementTable";
 import CustomerForm from "@/components/form/management/CustomerForm";
 import { TableMetadata } from "@/components/table/interface";
 import { useCustomers } from "@/components/table/hook";
 import { toLocaleDateTime } from "@/utils/time";
+import { Customer } from "../api/customer/interface";
+import { FormType } from "@/components/form/management/interface";
+
+type Data = Customer | null;
 
 const metadata: TableMetadata[] = [
   {
@@ -29,15 +34,65 @@ const metadata: TableMetadata[] = [
 ];
 
 const Page = () => {
+  const { data, fetcher, loading } = useCustomers();
+  const [selected, setSelected] = useState<Data>(null);
+  const [formType, setFormType] = useState<FormType>("create");
+  const [formModal, setFormModal] = useState<boolean>(false);
+
+  const onClose = () => {
+    setFormModal(false);
+    setSelected(null);
+  };
+
+  const onClickNewData = () => {
+    setFormType("create");
+    setSelected(null);
+    setFormModal(true);
+  };
+
+  const onClickWatchData = (data: Data) => {
+    setFormType("watch");
+    setSelected(data);
+    setFormModal(true);
+  };
+
+  const onClickEditData = (data: Data) => {
+    setFormType("edit");
+    setSelected(data);
+    setFormModal(true);
+  };
+
+  const onClickDeleteData = (data: Data) => {
+    setFormType("delete");
+    setSelected(data);
+    setFormModal(true);
+  };
+
   return (
     <Box>
-      <Typography variant="h6">客源管理</Typography>
+      <Typography variant="h6">管理員</Typography>
       <Divider />
-      <ManagementTable
-        title="客源管理"
-        metadata={metadata}
-        useData={useCustomers}
-        Form={CustomerForm}
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <ManagementTable<Customer>
+          title="用戶管理"
+          metadata={metadata}
+          datas={data}
+          onClickData={{
+            onNew: onClickNewData,
+            onWatch: onClickWatchData,
+            onEdit: onClickEditData,
+            onDelete: onClickDeleteData,
+          }}
+        />
+      )}
+      <CustomerForm
+        open={formModal}
+        type={formType}
+        data={selected}
+        onClose={onClose}
+        afterAction={fetcher}
       />
     </Box>
   );
